cb373e7279fe371746d05654371853b87e583d5a
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-16 00:03:58 -0500

    Implement the method checkVictory
    That checks whether a player wins

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,27 @@
 function GameBoard() {
   const gameBoard = { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9 };
+  // Check whether 3 cases have the same value
+  const checkRows = (elem1, elem2, elem3) => {
+    elem1 === elem2 ? elem2 === elem3 : false;
+  };
+  // Return true when one row has three cases that have the same value
+  const checkVictory = () => {
+    // Vertical
+    const array = [
+      // Vertical
+      checkRows(gameBoard[1], gameBoard[2], gameBoard[3]),
+      checkRows(gameBoard[4], gameBoard[5], gameBoard[6]),
+      checkRows(gameBoard[7], gameBoard[8], gameBoard[9]),
+      // Horizontals
+      checkRows(gameBoard[1], gameBoard[4], gameBoard[7]),
+      checkRows(gameBoard[2], gameBoard[5], gameBoard[8]),
+      checkRows(gameBoard[3], gameBoard[6], gameBoard[9]),
+      // Diagonal
+      checkRows(gameBoard[1], gameBoard[5], gameBoard[9]),
+      checkRows(gameBoard[7], gameBoard[5], gameBoard[3]),
+    ];
+    array.some(true);
+  };
   const show_board = () => {
     console.log(`
     ${gameBoard[1]} | ${gameBoard[2]} | ${gameBoard[3]}
