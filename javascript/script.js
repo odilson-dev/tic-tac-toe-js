e59ed52db6fa99864443fdbc6335304bb1b8f546
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-17 12:39:12 -0500

    Create and update board on screen

--- javascript/script.js
+++ javascript/script.js
@@ -1,37 +1,52 @@
 import { GameBoard } from "./gameboard.js";
+import { Player } from "./player.js";
 
 function screenController() {
   const boardDiv = document.querySelector(".board");
   const board = GameBoard();
+  const player1 = Player("John", "X");
+  const player2 = Player("Sophia", "O");
 
-  const theBoard = board.getGameBoard();
-  console.log(theBoard);
-  for (const key in theBoard) {
-    const value = theBoard[key];
-    const caseButton = document.createElement("button");
-    caseButton.textContent = value;
-    caseButton.classList.add("case");
-    boardDiv.appendChild(caseButton);
+  function createBoardOnScreen() {
+    const theBoard = board.getGameBoard();
+
+    for (const key in theBoard) {
+      const caseButton = document.createElement("button");
+      const value = theBoard[key];
+
+      caseButton.classList.add("case");
+      caseButton.id = value;
+      boardDiv.appendChild(caseButton);
+    }
+  }
+  createBoardOnScreen();
+
+  function updateBoard(buttonID, token) {
+    board.setGameBoard(buttonID, token);
+    const buttonToUpdate = document.getElementById(buttonID);
+    buttonToUpdate.textContent = token;
   }
 
-  for (const element of theBoard) {
-  }
+  let currentPlayer = player1;
+  const caseButtons = document.querySelectorAll(".case");
+
+  // while (!board.checkVictory()) {
+  //   //board.setGameBoard(currentPlayer.playRound(), currentPlayer.token);
+  //   updateBoard();
+  //   // Change the currentPlayer for each round
+  //   currentPlayer === player1
+  //     ? (currentPlayer = player2)
+  //     : (currentPlayer = player1);
+  // }
+
+  caseButtons.forEach((button) => {
+    button.addEventListener(
+      "click",
+      updateBoard(button.id, currentPlayer.token)
+    );
+  });
 }
 
 screenController();
 
-// board.show_board();
-
-// const player1 = Player("John", "X");
-// const player2 = Player("Sophia", "O");
-
-// let currentPlayer = player1;
-// while (!board.checkVictory()) {
-//   board.setGameBoard(currentPlayer.playRound(), currentPlayer.token);
-//   board.show_board();
-//   // Change the currentPlayer for each round
-//   currentPlayer === player1
-//     ? (currentPlayer = player2)
-//     : (currentPlayer = player1);
-// }
-// console.log("Game Over");
+console.log("Game Over");
