4ec735eb871db6058ef07c82bfb77835b7a4c1c5
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-02-19 11:08:59 -0500

    Allow players to have a score

--- javascript/player.js
+++ javascript/player.js
@@ -1,5 +1,8 @@
 // A player has name and a token
 export function Player(name, token) {
+  let score = 0;
+  const getScore = () => score;
+  const increaseScore = () => score++;
   const getName = () => name;
   const setName = (newName) => {
     name = newName;
@@ -8,5 +11,5 @@ export function Player(name, token) {
   const setToken = (newToken) => {
     token = newToken;
   };
-  return { getName, setName, getToken, setToken };
+  return { getName, setName, getToken, setToken, getScore, increaseScore };
 }
